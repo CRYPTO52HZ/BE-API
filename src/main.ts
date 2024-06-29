165143f760bf6cc8f1d90b8bf2a7e4529a014803
Gia Huy <huy.94.2016@gmail.com>
2024-06-29 22:53:35 +0700

    update : user module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {
+  BadRequestException,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Trading API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('crypto')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (ValidationError: ValidationError[] = []) => {
+        return new BadRequestException(ValidationError);
+      },
+      validationError: {
+        target: false,
+      },
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
